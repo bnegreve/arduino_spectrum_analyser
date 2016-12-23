5c7ad1b2ff8e8d4accb3f8400af030a1416afa48
Ben <ben@neb.dyn.cs.kuleuven.be>
2016-12-23 14:06:03 +0100

    Refactor fft file.

--- fft/fft.ino
+++ fft/fft.ino
@@ -1,139 +1,131 @@
 /*
   Spectrum analyser for Florian's LED display. Author Benjamin Negrevergne. 
   Based on example from the  FFT libray (Copyright (C) 2014 Enrique Condes). 
+  Author: Benjamin Negrevergne
 */
 
+#include <assert.h>
 #include "arduinoFFT.h"
 
-arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
-/* 
-These values can be changed in order to evaluate the functions 
-*/
+
+/* Global vars & defines  */
+
+//#define NDEBUG // Uncomment for production 
+#define DISPLAY_WIDTH 48
+#define DISPLAY_HEIGHT 8
+
+/* FFT variables */
 const uint16_t samples = 128; //This value MUST ALWAYS be a power of 2
-double signalFrequency = 1000;
-double samplingFrequency = 5000;
-uint8_t amplitude = 100;
-/* 
-These are the input and output vectors 
-Input vectors receive computed results from FFT
-*/
 double vReal[samples]; 
 double vImag[samples];
+arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 
+/* Required for speeding up the ADC */ 
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+/* Fake signal generation */
+#define GENERATE_FAKE_SIGNAL 1 //Generate fake signal instead of reading from ADC
+#define Theta 6.2831 //2*Pi
+double signalFrequency = 2000;
+double samplingFrequency = 5000;
+uint8_t amplitude = 50;
+
+
+/* Misc values for printVector */
 #define SCL_INDEX 0x00
 #define SCL_TIME 0x01
 #define SCL_FREQUENCY 0x02
 
+/*Timing */
+long t0;
 
 
+/* Main functions */ 
 
-#define Theta 6.2831 //2*Pi
+/* Build the encoded graph from the fft output data. 
+- The total number of columns in the graph is numCols * colWidth. 
+- numRows should be 8 for a display with 8 leds 
+*/
+void buildGraph(uint8_t *out, double *data,  uint8_t numBands, uint8_t numRows, uint8_t numCols, uint8_t colWidth); 
 
-void printSpectrum(double *vData, uint8_t size, uint8_t num_rows); 
+/* Print the output of build graph on Serial. Warning totalNumCols is numCols * colWidth (not just numCols). 
+   Does nothing if NDEBUG is defined 
+*/
+void printGraph(uint8_t *graphData, uint8_t numRows, uint8_t totalNumCols); 
 
 
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+/* Helper functions (do not export) */ 
+static double maxv(double *data, uint8_t size);
+static void startSampling();
+static void printSamplingInfo();
+static void printVector(double *vData, uint8_t bufferSize, uint8_t scaleType); 
+static uint8_t encodeBar(uint8_t val, uint8_t numRows); 
 
 void setup()
 {
+
+#ifndef NDEBUG
   Serial.begin(115200);
   Serial.println("Ready");
   pinMode(0, INPUT);
+#endif 
 
   /* Speedup the ADC */
   sbi(ADCSRA, ADPS2);
   cbi(ADCSRA, ADPS1);
   cbi(ADCSRA, ADPS0);
-
-
 }
 
 void loop() 
 {
-  /*
-  // Build raw data 
+  
+  startSampling(); 
+    
+  #ifdef GENERATE_FAKE_SIGNAL
   double cycles = (((samples-1) * signalFrequency) / samplingFrequency); //Number of signal cycles that the sampling will read
   for (uint8_t i = 0; i < samples; i++) 
   {
     vReal[i] = uint8_t((amplitude * (sin((i * (Theta * cycles)) / samples))) / 2.0);// Build data with positive and negative values
+    vImag[i] = 0;    
     //vReal[i] = uint8_t((amplitude * (sin((i * (6.2831 * cycles)) / samples) + 1.0)) / 2.0);// Build data displaced on the Y axis to include only positive values
   }
-  */
+  #else
+  for(uint8_t i = 0; i < samples; i++){
+    vReal[i] = (double)analogRead(0);
+    vImag[i] = 0;
+  }
+  #endif 
 
-
+  printSamplingInfo(); 
   
-    long t0, t;
-    t0 = micros(); 
-
-    for(uint8_t i = 0; i < samples; i++){
-      vReal[i] = (double)analogRead(0);
-      vImag[i] = 0;   
-      //delayMicroseconds(1000); // sample at 0.25Khz
-    }
-    
-    t = micros() - t0; 
-    Serial.println(t); 
-    Serial.println(1 / ((float)t/1.0E6)); 
-
-    /* samplingFrequency = (float)samples*1000/t;  */
-    /* for(uint8_t i = 0; i < samples; i++){ */
-    /*   Serial.println(vReal[i]);  */
-    /* } */
-  
-  Serial.println("Data:");
-  //PrintVector(vReal, samples, SCL_TIME);
+  //  Serial.println("Data:");
+  //  printVector(vReal, samples, SCL_TIME);
   FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
-  Serial.println("Weighed data:");
-  //PrintVector(vReal, samples, SCL_TIME);
+  //Serial.println("Weighed data:");
+  //printVector(vReal, samples, SCL_TIME);
   FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
-  Serial.println("Computed Real values:");
- // PrintVector(vReal, samples, SCL_INDEX);
-  Serial.println("Computed Imaginary values:");
- // PrintVector(vImag, samples, SCL_INDEX);
+  //  Serial.println("Computed Real values:");
+ // printVector(vReal, samples, SCL_INDEX);
+  //Serial.println("Computed Imaginary values:");
+ // printVector(vImag, samples, SCL_INDEX);
   FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
   Serial.println("Computed magnitudes:");
-  //PrintVector(vReal, (samples >> 1), SCL_FREQUENCY);  
-  //double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
-  //Serial.println(x, 6);
-    printSpectrum(vReal, (samples>>1), 8);
-  //  uint8_t bars[10];  
-//printLCDSpectrum(vReal, (samples>>1), bars, 50, 10); 
-  //while(1); /* Run Once */
-   delay(1000); /* Repeat after delay */
+  printVector(vReal, (samples >> 1), SCL_FREQUENCY);  
+
+  uint8_t bars[DISPLAY_WIDTH] = {0};  
+  uint8_t colWidth = 1; 
+  buildGraph(bars, vReal, samples/2, DISPLAY_HEIGHT, DISPLAY_WIDTH, colWidth);
+  printGraph(bars, 8, DISPLAY_WIDTH); 
+  delay(1000); /* Repeat after delay */
 }
 
-int maxv(double *vData, uint8_t size){
-  int max = 0;
-  for(int i = 0; i < size; i++){
-    max = vData[i]>max?vData[i]: max; 
+static uint8_t encodeBar(uint8_t val, uint8_t numRows){
+  if(val >= numRows) {
+    Serial.println((1<<numRows) - 1, HEX); 
+    return (1<<numRows) - 1; // all led on.
   }
-  return max; 
-}
 
-void printSpectrum(double *vData, uint8_t size, uint8_t num_rows){
-    int max = maxv(vData, size);
-    Serial.println();
-    for(int j = num_rows - 1; j >= 0 ; j--){
-      for(int i = 0; i < size; i++){
-        if(((float)vData[i] / max) * num_rows >= j)
-          Serial.print("#");
-        else
-          Serial.print(" ");
-      }
-      Serial.println();
-    }
-}
-
-
-void printBar(uint8_t len){
-  for(int i = 0; i < len; i ++)
-    Serial.print("#"); 
-  Serial.println(); 
-}
-
-
-uint8_t oneCol(uint8_t val){
   uint8_t res = 0;
   for(uint8_t i = 0; i < val; i++){
     res |= (1<<i); 
@@ -141,27 +133,111 @@ uint8_t oneCol(uint8_t val){
   return res; 
 }
 
-void printLCDSpectrum(double *vData, uint8_t size, uint8_t *out, uint8_t num_rows, uint8_t num_cols){
-    int max = maxv(vData, size);
-    //for(int j = num_rows - 1; j >= 0 ; j--){
-      int x = - 1; 
-      int sum = 0;
-      int count = 0;  
-      for(int i = 0; i < size; i++){
-        sum+= vData[i]; 
-        count++; 
-        if( x != (num_cols / i ) ){
-          x = i / num_cols;
-          printBar((float)(sum / count) / max * num_rows);
-          out[i] = (float)(sum / count) / max * num_rows;           
-          count = 0; 
-          sum = 0;         
-        }
+
+void buildGraph(uint8_t *out, double *data,  uint8_t numBands, uint8_t numRows, uint8_t numCols, uint8_t colWidth){
+
+  assert(numRows <= DISPLAY_HEIGHT); 
+  assert(numCols * colWidth <= DISPLAY_WIDTH); 
+
+
+  double scalingFactor = (numRows + 1) / maxv(data, numBands);  // used to bring all values back in the
+								// interval [0 - numRows] (inclusive because
+								// we can display 9 distinct values with 8 leds.)
+
+  /* Loop through display columns */
+  for(int i = 0; i < numCols; i++){
+    uint8_t bandStart = (float)i / numCols * numBands;
+    uint8_t bandEnd = (float)(i+1) / numCols * numBands;
+    double barHeight = 0; 
+    for(uint8_t j = bandStart; j < bandEnd; j++){
+      /* take the maximum value of all bands displayed by this column */ 
+      barHeight = data[j] > barHeight ? data[j] : barHeight; 
     }
+
+    #ifndef DNDEBUG
+    Serial.print("Col ");
+    Serial.print(i);
+    Serial.print(" : Band from ");
+    Serial.print(bandStart);
+    Serial.print(" to ");
+    Serial.print(bandEnd);
+    Serial.print(": ");
+    Serial.print(barHeight); 
+    Serial.println();
+    #endif 
+
+    for(uint8_t j = i * colWidth; j < (i+1) * colWidth; j++){
+      uint8_t x = barHeight * scalingFactor;
+      out[j] = encodeBar(barHeight * scalingFactor, numRows); 
+    }
+  }
 }
 
-void PrintVector(double *vData, uint8_t bufferSize, uint8_t scaleType) 
+void printGraph(uint8_t *graphData, uint8_t numRows, uint8_t totalNumCols){
+#ifndef NDEBUG
+  assert(totalNumCols <= DISPLAY_WIDTH); 
+  assert(numRows <= DISPLAY_HEIGHT); 
+
+  Serial.println("--------------- spectrum analyser input-------------------"); 
+  for(int i = 0; i < totalNumCols; i++){
+    Serial.print(graphData[i], HEX); 
+  }
+  Serial.println(); 
+
+  Serial.println("------------------ spectrum analyser ---------------------"); 
+  for(int i = numRows - 1 ; i >= 0; i--){
+    for(int j = 0; j < totalNumCols; j++){
+      if( graphData[j] & (1<<i) )
+      	Serial.print("#");
+      else
+      	Serial.print(" ");
+    }
+    Serial.println(); 
+  }
+  for(int i = 0; i < totalNumCols; i++)
+    Serial.print("-"); 
+  Serial.println();
+#endif
+}
+
+
+/* Compute the maximum value of a vector of size > 0 */
+static double maxv(double *data, uint8_t size){
+  assert(size != 0); 
+  double max = data[0];
+  for(int i = 1; i < size; i++){
+    max = data[i]>max?data[i]: max; 
+  }
+  return max; 
+}
+
+static void startSampling(){
+#ifndef NDEBUG
+    t0 = micros(); 
+#endif
+}
+static void printSamplingInfo(){
+#ifndef NDEBUG    
+    float t = (micros() - t0) / 1.0E3; // time (ms) 
+
+    Serial.print("Sampling time : ");
+    Serial.println(t);
+
+    Serial.print("Time per sample (ms) : ");
+    Serial.println(t/samples);
+
+    Serial.print("Sampling frequency (hz) : ");
+    Serial.println((1 / (t/1E3)) * samples);
+
+    Serial.print("Max ADC value: ");
+    Serial.println(maxv(vReal, samples)); 
+#endif
+}
+
+
+static void printVector(double *vData, uint8_t bufferSize, uint8_t scaleType) 
 {
+#ifndef NDEBUG
   for (uint16_t i = 0; i < bufferSize; i++) 
   {
     double abscissa;
@@ -184,4 +260,6 @@ void PrintVector(double *vData, uint8_t bufferSize, uint8_t scaleType)
     Serial.println();
   }
   Serial.println();
+#endif 
 }
+
